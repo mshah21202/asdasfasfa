232b96a57a24179eec8ecff025c85251082adac1
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2024-12-29 11:00:13 +0000

    changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page 5')),
+      appBar: AppBar(
+        title: const Text(
+          'Home Page 7',
+        ),
+      ),
       body: const SafeArea(
         child: Stack(
           fit: StackFit.expand,
