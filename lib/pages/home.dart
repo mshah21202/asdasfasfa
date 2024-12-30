395974d334c9b179806aea886bb652ab6115aad7
Mohamad Shahin <67559531+mshah21202@users.noreply.github.com>
2024-12-30 12:18:29 +0300

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Home',
+          'Home 5',
         ),
       ),
       body: const SafeArea(
