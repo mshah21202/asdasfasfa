6df0bff7ed62d261b1fc9efd430db676e7d98851
Mohamad Shahin <67559531+mshah21202@users.noreply.github.com>
2024-12-30 12:25:20 +0300

    Create hammoudeh_widget.dart

---
+++ lib/pages/widgets/hammoudeh_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:nowa_runtime/nowa_runtime.dart';
+
+@NowaGenerated()
+class HamoudehWidget extends StatelessWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const HamoudehWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Home Page 4',
+        ),
+      ),
+      body: const SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          alignment: Alignment(0, 0),
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
